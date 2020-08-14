3da3832ca9df730115ddcdb87b4cb70fea97f32a
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 13:56:37 +0200

    Add area class

---
+++ battleship/area.py
@@ -0,0 +1,16 @@
+"""
+Area module
+"""
+
+
+class Area:
+    """
+    Area class
+    """
+    def __init__(self, coordinate):
+        """
+
+        :param coordinate: tuple (x,y)
+        """
+        self.coordinate = coordinate
+        self.status = 1
