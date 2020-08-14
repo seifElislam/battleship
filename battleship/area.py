e03c8dfab81b1853b03253d458459dba48b93e65
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 15:03:16 +0200

    Change area to a node

--- battleship/area.py
+++ battleship/area.py
@@ -7,12 +7,14 @@ class Area:
     """
     Area class
     """
-    def __init__(self, x_coordinate, y_coordinate):
+    def __init__(self, x_coordinate, y_coordinate, next=None):
         """
 
         :param x_coordinate:
         :param y_coordinate:
+        :param next:
         """
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.status = 1
+        self.next = next
