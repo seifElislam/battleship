aca4b1059e8c2a2cd7a4c52c192508ed5f191c92
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 14:11:38 +0200

    Add game class

---
+++ battleship/game.py
@@ -0,0 +1,23 @@
+"""
+Game module
+"""
+from battleship.board import Board
+
+
+class Game:
+    """
+
+    """
+    def __init__(self, board_size):
+        """
+
+        :param board_size:
+        """
+        self.board_size = board_size
+
+    def add_ship(self):
+        pass
+
+    def verify_area(self):
+        pass
+
