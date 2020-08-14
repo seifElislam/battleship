513c22c989916a07656accdb469f88501a5d6529
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 13:59:41 +0200

    Add ship class

---
+++ battleship/ship.py
@@ -0,0 +1,23 @@
+"""
+Ship module
+"""
+
+
+class Ship:
+    """
+    Ship class
+    """
+    def __init__(self, areas):
+        """
+
+        :param areas:
+        """
+        self.areas = areas
+        self.status = 1
+
+    def check_status(self):
+        """
+
+        :return:
+        """
+        pass
