56746ca58785dc5aedfeb28a44549bb1848d252c
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 16:50:06 +0200

    Add getters & setters

--- battleship/area.py
+++ battleship/area.py
@@ -7,14 +7,53 @@ class Area:
     """
     Area class
     """
-    def __init__(self, x_coordinate, y_coordinate, next=None):
+    def __init__(self, x_coordinate, y_coordinate, status=1, next=None):
         """
 
         :param x_coordinate:
         :param y_coordinate:
+        :param status:
         :param next:
         """
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
-        self.status = 1
+        self._status = status
         self.next = next
+        self._belong_to_ship = False
+
+    def get_status(self):
+        """
+
+        :return:
+        """
+        return self._status
+
+    def set_status(self, status):
+        """
+
+        :param status:
+        :return:
+        """
+        self._status = status
+
+    def get_belong_to_ship(self):
+        """
+
+        :return:
+        """
+        return self._belong_to_ship
+
+    def set_belong_to_ship(self):
+        """
+
+        :return:
+        """
+        self._belong_to_ship = True
+
+    def __repr__(self):
+        """
+
+        :return:
+        """
+        return '({}, {})'.format(self.x_coordinate, self.y_coordinate)
+
