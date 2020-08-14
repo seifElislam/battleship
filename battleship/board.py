f609faf354612ba5d9feb55a1cb0e02096bb88a1
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 14:03:10 +0200

    Add board class

---
+++ battleship/board.py
@@ -0,0 +1,23 @@
+"""
+Board module
+"""
+
+
+class Board:
+    """
+
+    """
+    def __init__(self, size):
+        """
+
+        :param size:
+        """
+        self.size = size
+
+    def init_areas(self, size):
+        """
+
+        :param size:
+        :return:
+        """
+        pass
