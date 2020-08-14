4db7024abf3fa1d53652f079c87cf3cbf918d3a5
seifElislam <seif.eleslam1990@gmail.com>
2020-08-14 15:04:12 +0200

    Change ship class to circular linked list

--- battleship/ship.py
+++ battleship/ship.py
@@ -1,6 +1,7 @@
 """
 Ship module
 """
+from battleship.area import Area
 
 
 class Ship:
@@ -12,8 +13,35 @@ class Ship:
 
         :param areas:
         """
+        self.head = None
         self.areas = areas
         self.status = 1
+
+    def add_areas(self, areas):
+        """
+
+        :param areas:
+        :return:
+        """
+        for area in areas:
+            if area.next:
+                raise Exception
+            ptr1 = area
+            temp = self.head
+
+            ptr1.next = self.head
+
+            # If linked list is not None then set the next of
+            # last node
+            if self.head is not None:
+                while temp.next != self.head:
+                    temp = temp.next
+                temp.next = ptr1
+
+            else:
+                ptr1.next = ptr1  # For the first node
+
+            self.head = ptr1
 
     def check_status(self):
         """
